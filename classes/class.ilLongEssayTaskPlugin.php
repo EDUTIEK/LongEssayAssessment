f569f7e21bd39b43af18689464be261ca1baa0d5
Fred Neumann <fred.neumann@fau.de>
2022-09-01 01:11:17 +0200

    completed uninstall

--- classes/class.ilLongEssayTaskPlugin.php
+++ classes/class.ilLongEssayTaskPlugin.php
@@ -74,6 +74,7 @@ class ilLongEssayTaskPlugin extends ilRepositoryObjectPlugin
 		$tables = ["xlet_access_token", "xlet_alert", "xlet_corr_setting", "xlet_corrector", "xlet_corrector_ass",
 			"xlet_corrector_comment", "xlet_corrector_summary", "xlet_crit_points", "xlet_editor_comment",
 			"xlet_editor_history", "xlet_editor_notice", "xlet_editor_settings", "xlet_essay", "xlet_grade_level",
+            "xlet_log_entry",
 			"xlet_object_settings", "xlet_participant", "xlet_plugin_config", "xlet_rating_crit", "xlet_task_settings",
 			"xlet_time_extension", "xlet_writer_notice", "xlet_writer", "xlet_writer_comment", "xlet_writer_history",
             "xlet_resource"];

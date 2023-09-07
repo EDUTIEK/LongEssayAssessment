2f7281019c17da70fbe21f546426788a3d873159
Fred Neumann <fred.neumann@fau.de>
2023-09-07 10:50:18 +0200

    increased version number

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
 $id = 'xlas';
 
 // code version; must be changed for all code changes
-$version = '1.0.17';
+$version = '1.0.18';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '7.0';

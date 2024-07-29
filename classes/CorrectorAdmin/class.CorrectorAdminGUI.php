a10c61439de230ead0dc1a86526df586d2f6e3ae
fneumann <neumann@ilias.de>
2024-07-29 19:04:58 +0200

    Fix writing mails to correctors / to selected

--- classes/CorrectorAdmin/class.CorrectorAdminGUI.php
+++ classes/CorrectorAdmin/class.CorrectorAdminGUI.php
@@ -336,7 +336,7 @@ class CorrectorAdminGUI extends BaseGUI
         // Action
         $post = $this->request->getParsedBody();
         $correctors = [];
-        switch($post['form_input_1'] ?? '') {
+        switch($post['form/input_0'] ?? '') {
             case 'all':
                 $correctors = $all;
                 break;
@@ -385,11 +385,11 @@ class CorrectorAdminGUI extends BaseGUI
         $post = $this->request->getParsedBody();
         $to_writer = false;
         $to_correctors = [];
-        if (isset($post['form_input_1'])) {
+        if (isset($post['form/input_0'])) {
             $to_writer = true;
         }
         for ($i = 1; $i <= $this->settings->getRequiredCorrectors(); $i++) {
-            if (isset($post['form_input_' . ($i + 1)])) {
+            if (isset($post['form/input_' . ($i)])) {
                 $to_correctors[$i - 1] = true;
             }
         }

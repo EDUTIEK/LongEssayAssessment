729fa17f4d16a01b539b00c101ff5af450cdc762
fneumann <neumann@ilias.de>
2024-07-30 02:50:20 +0200

    Version 1.8

--- plugin.php
+++ plugin.php
@@ -5,10 +5,10 @@
 $id = 'xlas';
 
 // code version; must be changed for all code changes
-$version = '1.7.7';
+$version = '1.8';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '7.25';
+$ilias_min_version = '7.30';
 $ilias_max_version = '7.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email

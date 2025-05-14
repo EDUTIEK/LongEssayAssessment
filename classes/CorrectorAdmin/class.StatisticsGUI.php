c367c1db4d935a4610cd50c0e683dbadbd0f46cf
Fabian Wolf <fabian@eros.rudelnet.de>
2025-05-14 16:50:35 +0200

    Fix #45080 and prevent removed users from being exported

--- classes/CorrectorAdmin/class.StatisticsGUI.php
+++ classes/CorrectorAdmin/class.StatisticsGUI.php
@@ -38,7 +38,7 @@ abstract class StatisticsGUI extends BaseGUI
 
     protected function buildCSV(array $records, string $count_title, string $finalized_title, bool $has_obj_id = true) : string
     {
-        $grade_levels = array_unique(array_merge(...array_map(fn (array $x) => array_keys($x['grade_statistics']), $records)));
+        $grade_levels = array_unique(array_merge(...array_map(fn (array $x) => array_keys($x['grade_statistics'] ?? []), $records)));
 
         if($has_obj_id) {
             $obj_titles = [];
@@ -71,7 +71,7 @@ abstract class StatisticsGUI extends BaseGUI
         }
 
         foreach($records as $record) {
-            if(!isset($record["usr_id"])) {
+            if(!isset($record["usr_id"]) || ! ilObjUser::_exists($record["usr_id"])) {
                 continue;
             }
             $csv->addRow();

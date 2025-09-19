65b6d39c5195fd00a62e67f957db01dea492615a
Fabian Wolf <fabian@eros.rudelnet.de>
2025-09-19 14:38:17 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#CRON_PLUGIN_PATH=$(realpath -m ../../../Cron/CronHook/LongEssayAssessmentCron)
+CRON_PLUGIN_PATH=$(realpath -m ../../../Cron/CronHook/LongEssayAssessmentCron)
 UI_PLUGIN_PATH=$(realpath -m ../../../UIComponent/UserInterfaceHook/LongEssayAssessmentCollection)
 echo " Creating new folders and files for optional plugins..."
 
 echo -e " \033[32mInfo: This script operates outside of this directory.\033[0m";
 
-#rm -rf $CRON_PLUGIN_PATH/classes $UI_PLUGIN_PATH/classes
-#mkdir -p $CRON_PLUGIN_PATH/classes
+rm -rf $CRON_PLUGIN_PATH/ $UI_PLUGIN_PATH/classes
 mkdir -p $UI_PLUGIN_PATH/classes
 cp ./plugin.php $UI_PLUGIN_PATH/plugin.php
 sed -i -e 's/xlas/xlasu/' -e '/\/\/ features/,$d' $UI_PLUGIN_PATH/plugin.php

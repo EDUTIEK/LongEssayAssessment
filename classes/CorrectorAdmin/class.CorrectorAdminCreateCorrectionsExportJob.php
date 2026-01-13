848320e4692adb397ab6a31257e5ba84b2e5665f
fneumann <fneumann@databay.de>
2026-01-13 09:16:22 +0100

    Revert Anonymize ZIP export

--- classes/CorrectorAdmin/class.CorrectorAdminCreateCorrectionsExportJob.php
+++ classes/CorrectorAdmin/class.CorrectorAdminCreateCorrectionsExportJob.php
@@ -81,14 +81,14 @@ class CorrectorAdminCreateCorrectionsExportJob extends AbstractJob
                 if ($repoEssay->getWritingAuthorized()) {
                     $repoWriter = $this->writerRepo->getWriterById($repoEssay->getWriterId());
 
-                    $subdir = ilFileDelivery::returnASCIIFilename($repoWriter->getPseudonym());
+                    $subdir = ilFileDelivery::returnASCIIFilename($user_data_helper->getFullname($repoWriter->getUserId()) . ' (' . $user_data_helper->getLogin($repoWriter->getUserId()) . ')');
                     $storage->createDir($zipdir . '/' . $subdir);
 
                     $filename = $subdir . '-writing.pdf';
-                    $storage->write($zipdir . '/' . $subdir. '/'. $filename, $writerAdminService->getWritingAsPdf($object, $repoWriter, true));
+                    $storage->write($zipdir . '/' . $subdir. '/'. $filename, $writerAdminService->getWritingAsPdf($object, $repoWriter));
 
                     $filename = $subdir . '-correction.pdf';
-                    $storage->write($zipdir . '/' . $subdir. '/'. $filename, $correctorAdminService->getCorrectionAsPdf($object, $repoWriter, null, false, true));
+                    $storage->write($zipdir . '/' . $subdir. '/'. $filename, $correctorAdminService->getCorrectionAsPdf($object, $repoWriter));
                 }
             }
 

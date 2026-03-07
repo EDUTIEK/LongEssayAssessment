2cd7716eeeddcf5468ca0de804fc8f7e53d490a1
fneumann <neumann@ilias.de>
2026-03-07 23:57:23 +0100

    Create Documentation export

---
+++ src/Export/ExportOptionDocumentation.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ILIAS\Plugin\LongEssayAssessment;
+
+use Edutiek\AssessmentService\Assessment\Data\ExportType;
+
+class ExportOptionDocumentation extends ExportOption
+{
+    public function getServiceExportType(): ExportType
+    {
+        return ExportType::DOCUMENTATION;
+    }
+}
